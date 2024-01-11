dca5e54d69fe3457e42e9f1b0430d54b6ea3b0b7
jpetterson88 <jpmaverick.rj@gmail.com>
2024-01-11 03:11:15 -0300

    Aula 04 - Adiciona busca do pokémon

--- pokedex/src/js/script.js
+++ pokedex/src/js/script.js
@@ -1,16 +1,30 @@
 const lista_pokemons = document.getElementById("pokemons");
 
-const getPokemons = () => {
-  const pokemon_api = fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-  
-  pokemon_api.then(response => {
-    return response.json();
-  }).then(data => {
-    render(data.results);
-  });
+const getPokemons = async (name= "") => {
+  if (name !== "") {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const data = await response.json();
+    renderOne(data);
+  } else {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const data = await response.json();
+    renderAll(data.results);
+  }
 }
 
-const render = (pokemons) => {
+const renderOne = (pokemon) => {
+  lista_pokemons.innerHTML = "";
+  lista_pokemons.insertAdjacentHTML("beforeend", 
+  `<li class="pokemon-card">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.id}.gif" />
+    <h3>${pokemon.name}</h3>
+    <h4>Peso: ${pokemon.weight}</h4>
+    <p>Tipo:${pokemon.types.map(item => " " + item.type.name)}</p>
+  </li>`);
+}
+
+const renderAll = (pokemons) => {
+  lista_pokemons.innerHTML = "";
   pokemons.map((pokemon, index) => {
     lista_pokemons.insertAdjacentHTML("beforeend", 
     `<li class="pokemon-card">
@@ -18,6 +32,13 @@ const render = (pokemons) => {
       <h3>${pokemon.name}</h3>
     </li>`)
   });
+}
+
+const searchPokemon = (event) => {
+  // previne o comportamento padrao do evento submit (atualizar a pagina)
+  event.preventDefault();
+  const text = document.getElementById("buscarInput").value;
+  getPokemons(text);
 }
 
 getPokemons();
